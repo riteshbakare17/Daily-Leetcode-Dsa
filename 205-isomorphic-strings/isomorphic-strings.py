c7ea88122aabe4afc5d39779bbef9e96184ce611
Ritesh Bakare <riteshbakare7241@gmail.com>
2026-08-29 23:57:22 +0530

    Time: 4 ms (61.86%) | Memory: 19.2 MB (83.17%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isIsomorphic(self, s, t):
+        st = {}
+        ts = {}
+
+        for a, b in zip(s, t):
+            if a in st and st[a] != b:
+                return False
+
+            if b in ts and ts[b] != a:
+                return False
+
+            st[a] = b
+            ts[b] = a
+
+        return True
